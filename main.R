49cf1efc410d7e0e6b60b4be7be1d5255d1ee4a8
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2013-11-09 18:24:05 +0000

    Now retweets are included in the tweets word cloud

--- main.R
+++ main.R
@@ -27,7 +27,7 @@ fetchFollowingDescriptions <- function (username = "orfc") {
 }
 
 fetchTweets <- function (username = "orfc") {
-    tweets <- userTimeline(username, n=3200)
+    tweets <- userTimeline(username, n = 3200, includeRts = TRUE)
     words <- Reduce(paste, sapply(tweets, function (x) { x$text }))
     # I remove all URLs (see http://stackoverflow.com/q/16003852/1218376 )
     words <- gsub("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+", "", words)
@@ -38,6 +38,7 @@ fetchTweets <- function (username = "orfc") {
 makeWordcloud <- function (words, filename = "wordcloud.png") {
     # thanks to http://onertipaday.blogspot.co.uk/2011/07/word-cloud-in-r.html
     # although it does not really explain what happens here :-(
+    require(tm)
     require(RColorBrewer)
     require(wordcloud)
     words <- gsub("[^A-Za-z0-9 #]", "", words)
@@ -52,6 +53,7 @@ makeWordcloud <- function (words, filename = "wordcloud.png") {
     d <- data.frame(word = names(v), freq = v)
     pal <- brewer.pal(8, "Dark2")
     png(filename, width = 1280, height = 800)
-    wordcloud(d$word, d$freq, scale = c(8, 1), min.freq = 2, max.words = 100, random.order = T, rot.per = .15, colors = pal)
+    topWordMagnification <- 12
+    wordcloud(d$word, d$freq, scale = c(topWordMagnification, topWordMagnification / 8), min.freq = 2, max.words = 100, random.order = T, rot.per = .15, colors = pal)
     dev.off()
 }
